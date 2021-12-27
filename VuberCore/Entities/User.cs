9e02da0da26eca476d281698cd4797301804842e
Edgar Rakhmankulov <oakmillennial@gmail.com>
2021-12-27 20:26:09 +0300

    fix Username, Name and Rating properties to User

--- VuberCore/Entities/User.cs
+++ VuberCore/Entities/User.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VuberCore.Entities
 {
     public abstract class User : Entity
     {
-
+        [Required]
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public Rating Rating { get; set; }
+        
     }
 }
