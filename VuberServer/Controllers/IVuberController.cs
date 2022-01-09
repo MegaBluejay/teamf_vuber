52e9180537db1f28552f2bcaa6fc535d11b28d79
den26012002 <den26012002@mail.ru>
2022-01-09 22:09:13 +0300

    feat: add rides watching

--- VuberServer/Controllers/IVuberController.cs
+++ VuberServer/Controllers/IVuberController.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using VuberCore.Entities;
 
 namespace VuberServer.Controllers
 {
     public interface IVuberController
     {
+        List<Ride> SeeRides(User activeUser);
         void SetRating(Rating rating, Guid userId);
     }
 }
--- VuberServer/Controllers/VuberController.cs
+++ VuberServer/Controllers/VuberController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.SignalR;
 using VuberServer.Hubs;
@@ -13,10 +14,16 @@ namespace VuberServer.Controllers
         private readonly IHubContext<DriverHub> _driverHubContext;
         private readonly VuberDbContext _vuberDbContext;
 
-        public VuberController(IHubContext<ClientHub> clientHubContext, IHubContext<DriverHub> driverHubContext)
+        public VuberController(IHubContext<ClientHub> clientHubContext, IHubContext<DriverHub> driverHubContext, VuberDbContext vuberDbContext)
         {
             _clientHubContext = clientHubContext ?? throw new ArgumentNullException(nameof(clientHubContext));
             _driverHubContext = driverHubContext ?? throw new ArgumentNullException(nameof(driverHubContext));
+            _vuberDbContext = vuberDbContext;
+        }
+
+        public List<Ride> SeeRides(User activeUser)
+        {
+            return _vuberDbContext.Clients.FirstOrDefault(user => user.Id == activeUser.Id).Rides;
         }
 
         public void SetRating(Rating rating, Guid userId)
