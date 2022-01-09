276638bfdadddb22dc604d577ca5f1c11a5a5f29
Lumograul <lumograul@gmail.com>
2022-01-09 22:44:26 +0300

    feat: added CreateNewRide and CalculatePrice methods

--- VuberServer/Controllers/IVuberController.cs
+++ VuberServer/Controllers/IVuberController.cs
@@ -1,12 +1,20 @@
 using System;
 using System.Collections.Generic;
 using VuberCore.Entities;
+using Geolocation;
 
 namespace VuberServer.Controllers
 {
     public interface IVuberController
     {
+        Ride CreateNewRide(
+            Client client, 
+            Coordinate startLocation, 
+            ICollection<Coordinate> targetLocations,
+            RideType rideType);
+        
         List<Ride> SeeRides(User activeUser);
+        
         void SetRating(Rating rating, Guid userId);
     }
 }
--- VuberServer/Controllers/VuberController.cs
+++ VuberServer/Controllers/VuberController.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Geolocation;
 using Microsoft.AspNetCore.SignalR;
-using VuberServer.Hubs;
 using VuberCore.Entities;
+using VuberServer.Hubs;
 using VuberCore.Data;
 
 namespace VuberServer.Controllers
@@ -13,12 +14,18 @@ namespace VuberServer.Controllers
         private readonly IHubContext<ClientHub> _clientHubContext;
         private readonly IHubContext<DriverHub> _driverHubContext;
         private readonly VuberDbContext _vuberDbContext;
-
+        private const decimal EconomyRideTypePriceMultiplier = 50;
+        private const decimal ComfortRideTypePriceMultiplier = 100;
+        private const decimal BusinessRideTypePriceMultiplier = 200;
+        private const decimal LoadedLevelExtraCharge = 100;
+        private WorkloadLevel WorkloadLevel;
+        
         public VuberController(IHubContext<ClientHub> clientHubContext, IHubContext<DriverHub> driverHubContext, VuberDbContext vuberDbContext)
         {
             _clientHubContext = clientHubContext ?? throw new ArgumentNullException(nameof(clientHubContext));
             _driverHubContext = driverHubContext ?? throw new ArgumentNullException(nameof(driverHubContext));
             _vuberDbContext = vuberDbContext;
+            WorkloadLevel = WorkloadLevel.Normal;
         }
 
         public List<Ride> SeeRides(User activeUser)
@@ -28,9 +35,56 @@ namespace VuberServer.Controllers
 
         public void SetRating(Rating rating, Guid userId)
         {
-            User user = _vuberDbContext.Users.FirstOrDefault(user => user.Id == userId);
-            user.Rating = rating;
+            User userToSetRating = _vuberDbContext.Users.FirstOrDefault(user => user.Id == userId) ?? throw new ArgumentNullException();
+            userToSetRating.Rating = rating;
             _vuberDbContext.SaveChanges();
+        }
+
+        public Ride CreateNewRide(Client client, Coordinate startLocation, ICollection<Coordinate> targetLocations, RideType rideType)
+        {
+            return new Ride()
+            {
+                Client = client, 
+                Cost = CalculatePrice(rideType, startLocation, targetLocations), 
+                RideType = rideType, 
+                Status = RideStatus.Looking, 
+                StartLocation = startLocation, 
+                TargetLocations = targetLocations, 
+                Created = DateTime.UtcNow,
+            };
+        }
+
+        private decimal CalculatePrice(RideType rideType, Coordinate startLocation, ICollection<Coordinate> targetLocations)
+        {
+            double length = Math.Sqrt(Math.Pow(startLocation.Latitude - targetLocations.First().Latitude, 2)
+                                      + Math.Pow(startLocation.Longitude - targetLocations.First().Longitude, 2));
+            if (targetLocations.Count > 1)
+            {
+                List<Coordinate> listOfTargetLocations = targetLocations.ToList();
+                for (int i = 0; i < listOfTargetLocations.Count - 1; i++)
+                {
+                    Coordinate location1 = listOfTargetLocations[i];
+                    Coordinate location2 = listOfTargetLocations[i + 1];
+                    length += Math.Sqrt(Math.Pow(location1.Latitude - location2.Latitude, 2)
+                                        + Math.Pow(location1.Longitude - location2.Longitude, 2));
+                }
+            }
+
+            var price = Convert.ToDecimal(length);
+            switch (rideType)
+            {
+                case RideType.Economy:
+                    price = price * EconomyRideTypePriceMultiplier + LoadedLevelExtraCharge;
+                    break;
+                case RideType.Comfort:
+                    price = price * ComfortRideTypePriceMultiplier + LoadedLevelExtraCharge;
+                    break;
+                case RideType.Business:
+                    price = price * BusinessRideTypePriceMultiplier + LoadedLevelExtraCharge;
+                    break;
+            }
+
+            return price;
         }
     }
 }
