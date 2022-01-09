e15ea8c909358c9506bd322e61c6bd4adcc3fe04
Lumograul <lumograul@gmail.com>
2022-01-09 21:01:37 +0300

    feat: added dto's RideToClient.cs and RideToDriver.cs

---
+++ VuberCore/Dto/RideToClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Geolocation;
+using VuberCore.Entities;
+
+namespace VuberCore.Dto
+{
+    public class RideToClient
+    {
+        public Coordinate StartLocation { get; set; }
+        public ICollection<Coordinate> TargetLocations { get; set; }
+        public RideLevel RideLevel { get; set; }
+        public RideStatus Status { get; set; }
+        public Rating DriverRating { get; set; }
+    }
+}
---
+++ VuberCore/Dto/RideToDriver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Geolocation;
+using VuberCore.Entities;
+
+namespace VuberCore.Dto
+{
+    public class RideToDriver
+    {
+        public Coordinate StartLocation { get; set; }
+        public ICollection<Coordinate> TargetLocations { get; set; }
+        public RideLevel RideLevel { get; set; }
+        public RideStatus Status { get; set; }
+        public Rating ClientRating { get; set; }
+    }
+}
