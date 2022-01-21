94fae79934ed53eaf8c0ac9fe6842cb939494670
MegaBluejay <dmoiseev2011@gmail.com>
2022-01-21 20:18:38 +0300

    add DbContext service to Startup

--- VuberServer/Startup.cs
+++ VuberServer/Startup.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VuberServer.Data;
 using VuberServer.Hubs;
 
 namespace VuberServer
@@ -25,8 +28,13 @@ namespace VuberServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionStringSettings = ConfigurationManager.ConnectionStrings["VuberDatabase"];
             services.AddRazorPages();
             services.AddSignalR();
+            services.AddDbContext<VuberDbContext>(options =>
+                options
+                    .UseLazyLoadingProxies()
+                    .UseNpgsql(connectionStringSettings.ConnectionString));
             services.AddSingleton<IUserIdProvider, VuberUserIdProvider>();
         }
 
