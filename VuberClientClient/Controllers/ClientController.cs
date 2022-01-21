523b51fc5c273b98d8c141143bd67971d2d0ca34
den26012002 <den26012002@mail.ru>
2022-01-21 20:11:21 +0300

    feat: add client controller functions without implementation

--- VuberClientClient/Controllers/ClientController.cs
+++ VuberClientClient/Controllers/ClientController.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Geolocation;
+using VuberCore.Entities;
 
 namespace VuberClientClient.Controllers
 {
@@ -8,6 +12,48 @@ namespace VuberClientClient.Controllers
     {
         public ClientController() // сюда нужно передать hub connection
         {
+        }
+
+        [HttpPost]
+        [Route("createOrder")]
+        public IActionResult CreateOrder([FromQuery] Coordinate startLocation, [FromQuery] ICollection<Coordinate> targetLocations, [FromQuery] RideType rideType, PaymentType paymentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("cancelOrder")]
+        public IActionResult CancelOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("addPayementCard")]
+        public IActionResult AddPaymentCard(string cardData)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("seeRides")]
+        public IActionResult SeeRides()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("setRating")]
+        public IActionResult SetRating([FromQuery] double value, [FromQuery] Guid driverId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("getDriverRating")]
+        public IActionResult GetDriverRating(Guid driverId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
