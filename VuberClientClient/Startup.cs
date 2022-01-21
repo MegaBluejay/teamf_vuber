e66a37e70eb2663a3016649df645422b646ba2d5
den26012002 <den26012002@mail.ru>
2022-01-21 21:03:28 +0300

    refactor: delete environment IsDevelopment checks

--- VuberClientClient/Startup.cs
+++ VuberClientClient/Startup.cs
@@ -42,12 +42,9 @@ namespace VuberClientClient
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "VuberClientClient v1"));
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "VuberClientClient v1"));
 
             app.UseHttpsRedirection();
 
