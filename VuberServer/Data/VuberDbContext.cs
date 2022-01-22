c99e587f834ceb37ab7bff25faadf5f25e85bfc6
Edgar Rakhmankulov <oakmillennial@gmail.com>
2022-01-22 16:24:49 +0300

    add entities to db

--- VuberServer/Data/VuberDbContext.cs
+++ VuberServer/Data/VuberDbContext.cs
@@ -11,5 +11,9 @@ namespace VuberServer.Data
         public DbSet<Client> Clients { get; set; }
         public DbSet<Driver> Drivers { get; set; }
         public DbSet<Ride> Rides { get; set; }
+        
+        public DbSet<Checkpoint> Checkpoints { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<PaymentCard> PaymentCards { get; set; }
     }
 }
