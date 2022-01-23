5a0253455f9d3dcfc79ef574b999ba6d6a616ac4
MegaBluejay <dmoiseev2011@gmail.com>
2022-01-23 13:53:22 +0300

    add authorization header, remove unneeded services

--- VuberClientClient/Startup.cs
+++ VuberClientClient/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SignalR.Client;
@@ -33,8 +34,10 @@ namespace VuberClientClient
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "VuberClientClient", Version = "v1" });
             });
+            var creds = Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1")
+                .GetBytes("SomeUserName" + ":" + "__NOPASS__"));
             var hubConnection = new HubConnectionBuilder()
-                .WithUrl("localhost/client")
+                .WithUrl("http://localhost/client", options => options.Headers["Authorization"] = $"Basic {creds}")
                 .WithAutomaticReconnect()
                 .Build();
             var clientNotificationController = new ClientNotificationController();
--- VuberServer/Startup.cs
+++ VuberServer/Startup.cs
@@ -28,7 +28,6 @@ namespace VuberServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
             services.AddSignalR();
             var connectionString = Configuration.GetConnectionString("VuberDatabase");
             services.AddDbContext<VuberDbContext>(options =>
@@ -44,20 +43,6 @@ namespace VuberServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
             app.UseRouting();
 
             app.UseAuthentication();
