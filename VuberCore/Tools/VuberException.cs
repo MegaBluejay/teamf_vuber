4e1253ee312c2fa66324e508a80ef40d2a36e453
den26012002 <den26012002@mail.ru>
2022-01-23 17:21:15 +0300

    feat: add VuberException

---
+++ VuberCore/Tools/VuberException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuberCore.Tools
+{
+    public class VuberException : Exception
+    {
+        public VuberException()
+        {
+        }
+
+        public VuberException(string message)
+            : base(message)
+        {
+        }
+
+        public VuberException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
